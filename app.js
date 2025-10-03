e9c660d070d99be06baa18528ffb48b05ad0a097
Bon Design <info.jakub.stefan@gmial.com>
2025-10-03 13:28:46 +0200

    Animacje

--- app.js
+++ app.js
@@ -53,3 +53,20 @@ formResult.textContent = 'Wiadomość wysłana! (symulacja)';
 formResult.style.color = '#00ffcc';
 contactForm.reset();
 });
+
+const sections = document.querySelectorAll('section'); 
+
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){ 
+      entry.target.classList.add('show'); 
+    }
+  });
+}, { threshold: 0.2 }); 
+
+
+sections.forEach(section => {
+  section.classList.add('hidden'); 
+  observer.observe(section); 
+});
